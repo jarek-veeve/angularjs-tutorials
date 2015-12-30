e51f40d042f63f334beb65935e07ce0c823b8afb
jarek <jaroslaw.jasinski89@gmail.com>
2015-12-30 15:13:33 +0000

    New tutorials from book.

---
+++ src/scripts/from-book/tutorial1.js
@@ -0,0 +1,26 @@
+var firstTutorial = angular.module('firstTutorial', [])
+
+    firstTutorial.controller('TutorialController', function($scope){
+       
+        $scope.first = "First";
+        $scope.last = "Last";
+        
+        $scope.heading = "Message: ";
+        
+        $scope.updateMessage = function() {
+            $scope.message = 'Hello ' + $scope.first + ' ' + $scope.last + '!';
+        }
+        
+        $scope.resetMessage = function() {
+            $scope.message = ' ';
+            $scope.first = "First";
+            $scope.last = "Last";
+        }
+        
+        $scope.clicked = function(event) {
+            $scope.first = "First2222";
+            
+        }
+    
+    });
+
